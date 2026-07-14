e97c5ec39723d1574d3960d2cf34eb688a7732fd
lanstheprodigy <alan.040219.gam@gmail.com>
2026-07-14 22:48:17 +0700

    Enhance user authentication flow; implement password reset functionality and improve error handling for login attempts.

---
+++ prisma/migrations/20260714154804/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `sessionTimeLimits` on the `TestPackage` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "TestPackage" DROP COLUMN "sessionTimeLimits";
